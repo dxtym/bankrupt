153582117d24e9ac223ab72bafac7630b636292b
Dilmurod <100064552+dxtym@users.noreply.github.com>
2024-06-12 23:02:32 +0500

    feat: add optional params (#14)

--- Makefile
+++ Makefile
@@ -49,10 +49,10 @@ proto:
 	rm -rf pb/*.go
 	rm -rf doc/swagger/*.swagger.json
 	protoc --proto_path=proto --go_out=pb --go_opt=paths=source_relative \
-  --go-grpc_out=pb --go-grpc_opt=paths=source_relative \
+  	--go-grpc_out=pb --go-grpc_opt=paths=source_relative \
 	--grpc-gateway_out=pb --grpc-gateway_opt=paths=source_relative \
 	--openapiv2_out=doc/swagger --openapiv2_opt=allow_merge=true,merge_file_name=bankrupt \
-  proto/*.proto
+  	proto/*.proto
 	statik -f -src=./doc/swagger -dest=./doc
 
 evans:
--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 	"time"
 
@@ -52,4 +53,89 @@ func TestGetUser(t *testing.T) {
 	require.Equal(t, user1.Email, user2.Email)
 	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
 	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+}
+
+func TestUpdateUserOnlyFullName(t *testing.T) {
+	user := createRandomUser(t)
+	newFullName := utils.RandomOwner()
+	updatedUser, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+		Username: user.Username,
+		FullName: sql.NullString{
+			String: newFullName,
+			Valid:  true,
+		},
+	})
+
+	require.NoError(t, err)
+	require.Equal(t, user.Username, updatedUser.Username)
+	require.Equal(t, user.HashedPassword, updatedUser.HashedPassword)
+	require.Equal(t, newFullName, updatedUser.FullName)
+	require.Equal(t, user.Email, updatedUser.Email)
+}
+
+func TestUpdateUserOnlyEmail(t *testing.T) {
+	user := createRandomUser(t)
+	newEmail := utils.RandomEmail()
+	updatedUser, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+		Username: user.Username,
+		Email: sql.NullString{
+			String: newEmail,
+			Valid:  true,
+		},
+	})
+
+	require.NoError(t, err)
+	require.Equal(t, user.Username, updatedUser.Username)
+	require.Equal(t, user.HashedPassword, updatedUser.HashedPassword)
+	require.Equal(t, user.FullName, updatedUser.FullName)
+	require.Equal(t, newEmail, updatedUser.Email)
+}
+
+func TestUpdateUserOnlyPassword(t *testing.T) {
+	user := createRandomUser(t)
+	newPassword, _ := utils.HashPassword(utils.RandomString(6))
+	updatedUser, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+		Username: user.Username,
+		HashedPassword: sql.NullString{
+			String: newPassword,
+			Valid:  true,
+		},
+	})
+
+	require.NoError(t, err)
+	require.Equal(t, user.Username, updatedUser.Username)
+	require.Equal(t, newPassword, updatedUser.HashedPassword)
+	require.Equal(t, user.FullName, updatedUser.FullName)
+	require.Equal(t, user.Email, updatedUser.Email)
+}
+
+func TestUpdateUserAllFields(t *testing.T) {
+	user := createRandomUser(t)
+	newFullName := utils.RandomOwner()
+	newEmail := utils.RandomEmail()
+	newPassword, _ := utils.HashPassword(utils.RandomString(6))
+	updatedUser, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+		Username: user.Username,
+		FullName: sql.NullString{
+			String: newFullName,
+			Valid:  true,
+		},
+		Email: sql.NullString{
+			String: newEmail,
+			Valid:  true,
+		},
+		HashedPassword: sql.NullString{
+			String: newPassword,
+			Valid:  true,
+		},
+	})
+
+	require.NoError(t, err)
+	require.Equal(t, user.Username, updatedUser.Username)
+	require.Equal(t, newPassword, updatedUser.HashedPassword)
+	require.NotEqual(t, user.HashedPassword, updatedUser.HashedPassword)
+	require.Equal(t, newFullName, updatedUser.FullName)
+	require.NotEqual(t, user.FullName, updatedUser.FullName)
+	require.Equal(t, newEmail, updatedUser.Email)
+	require.NotEqual(t, user.Email, updatedUser.Email)
 }
